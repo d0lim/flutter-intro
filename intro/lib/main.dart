9243687a90e14df25a42e4c5723992c0b545c962
DoLim <limdongyoung0@gmail.com>
2020-03-28 10:51:24 +0000

    Make our own sample Code

--- intro/lib/main.dart
+++ intro/lib/main.dart
@@ -40,6 +40,7 @@ class HelloPage extends StatefulWidget {
 
 class _HelloPageState extends State<HelloPage> {
   String _message = 'Hello World!';
+  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -51,9 +52,20 @@ class _HelloPageState extends State<HelloPage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Text(
-        _message,
-        style: TextStyle(fontSize: 30),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _message,
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              '$_counter',
+              style: TextStyle(fontSize: 30),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -61,6 +73,7 @@ class _HelloPageState extends State<HelloPage> {
   void _changeMessage() {
     setState(() {
       _message = 'Bye World';
+      _counter++;
     });
   }
 }
